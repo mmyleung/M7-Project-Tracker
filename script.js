5650d0ab7026b1989d7a237d32d150b4ec8e3c28
mmyleung <minamleung@gmail.com>
2023-05-18 11:21:55 +0100

    setInterval to update time every second

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+//set interval function to display time every second
+
+setInterval(function() {
 //save today's date and time into variable today
 let today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-
 //target span to display time
 let timeDisplay = $("#timeDisplay");
-
 //display time and date into timeDisplay
 timeDisplay.text(today);
-
-//
+}, 1000)
