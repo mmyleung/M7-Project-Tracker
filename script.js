665c67b2a833cdf5c6bf698e4033fdc6e6ce3aa2
mmyleung <minamleung@gmail.com>
2023-05-18 11:19:02 +0100

    add header, display date and time using moment API

---
+++ script.js
@@ -0,0 +1,10 @@
+//save today's date and time into variable today
+let today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+//target span to display time
+let timeDisplay = $("#timeDisplay");
+
+//display time and date into timeDisplay
+timeDisplay.text(today);
+
+//
