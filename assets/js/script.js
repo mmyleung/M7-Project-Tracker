34f2ad82734673ad78ff141310dc6e6878d6d89e
mmyleung <minamleung@gmail.com>
2023-05-18 14:29:32 +0100

    add table rows dynamically depending on number of table header present

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,35 @@ let timeDisplay = $("#timeDisplay");
 timeDisplay.text(today);
 }, 1000)
 
+//date picker in due date form
 $( function() {
-    $( "#dueDateInput" ).datepicker();
+    $( "#dueDateInput" ).datepicker({minDate: +1});
   } );
+
+//target the form input fields and store as variables
+var submitBtn = $("#submitBtn");
+var projectForm = $("#projectForm");
+var projectName = $("#projectNameInput").val();
+var projectType = $("#projectTypeSelect").val();
+var hourlyRate = $("#hourlyRateInput").val();
+var dueDate = $("#dueDateInput").val();
+var table = $("#formTable");
+var tableHeaderChildren = $("th");
+
+
+//target submit on project form
+projectForm.on("submit", function(event) {
+
+  //prevent default on form
+  event.preventDefault();
+  //prevent propagation
+  event.stopPropagation();
+  //add new table row
+  let tableRow = $("<tr>");
+  tableRow.appendTo($("#tableData"))
+  //for loop to generate the table cells
+ for (let i = 0; i < tableHeaderChildren.length; i++) {
+  let tableCell = $("<td>");
+  tableCell.appendTo(tableRow);
+ }
+})
